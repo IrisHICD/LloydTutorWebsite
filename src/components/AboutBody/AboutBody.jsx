f9cff7946d344dc7fd970c676e09e4a3b5bba10f
Iris Cheang-Deis <131198414+IrisHICD@users.noreply.github.com>
2024-12-03 14:57:39 -0700

    apply tablet style to about section

--- src/components/AboutBody/AboutBody.jsx
+++ src/components/AboutBody/AboutBody.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { animateScroll as scroll } from "react-scroll";
 import "./AboutBody.scss";
 import tutor from "../../assets/images/tutor.jpg";
+import tutortablet from "../../assets/images/tutor-tablet.jpg";
 
 function AboutBody() {
   const scrollToTop = () => {
@@ -21,39 +22,40 @@ function AboutBody() {
             &gt;
           </button>
         </div>
-
-        <p>
-          Hello, I’m Iris! With over a decade of tutoring experience, I’ve had
-          the privilege of inspiring students in STEM subjects. I’ve devoted my
-          evenings and weekends to tutoring because I’m passionate about
-          empowering young minds to achieve their potential. Parents often tell
-          me that my positive and engaging teaching style makes a difference,
-          helping students not only understand challenging concepts but also
-          build the confidence they need to thrive academically. There's nothing
-          more rewarding than seeing a student’s “lightbulb moment” and knowing
-          they’re on the path to success. I’d be delighted to support your
-          child’s learning journey. Let’s work together to help them reach their
-          goals!
-        </p>
-      </div>
-      <div className="about__summary">
-        <div className="about__imgbox">
-          <img className="about__img" src={tutor} alt="Tutor Iris" />
-        </div>
-        <div className="about__details">
-          <p className="about__exp">
-            <strong>Tutor Experience:</strong> 10+ years{" "}
-          </p>
-          <p>
-            <strong>Bachelor of Science degree:</strong> University of Toronto
-            (Double Major in Genetics and Ecology)
-          </p>
-          <p>
-            <strong>Master of Science degree:</strong> University of
-            Saskatchewan (Research in Cattle Supplementation){" "}
-          </p>
-          <p className="about__specialty">
-            <strong>Specialty:</strong> Math and Sciences{" "}
+        <div className="about__tab">
+          <div className="about__summary">
+            <div className="about__imgbox">
+              <img className="about__img" src={tutor} alt="Tutor Iris" />
+            </div>
+            <div className="about__details">
+              <p className="about__exp">
+                <strong>Tutor Experience:</strong> 10+ years{" "}
+              </p>
+              <p>
+                <strong>Bachelor of Science degree:</strong> University of
+                Toronto (Double Major in Genetics and Ecology)
+              </p>
+              <p>
+                <strong>Master of Science degree:</strong> University of
+                Saskatchewan (Research in Cattle Supplementation){" "}
+              </p>
+              <p className="about__specialty">
+                <strong>Specialty:</strong> Math and Sciences{" "}
+              </p>
+            </div>
+          </div>
+          <p className="about__para">
+            Hello, I’m Iris! With over a decade of tutoring experience, I’ve had
+            the privilege of inspiring students in STEM subjects. I’ve devoted
+            my evenings and weekends to tutoring because I’m passionate about
+            empowering young minds to achieve their potential. Parents often
+            tell me that my positive and engaging teaching style makes a
+            difference, helping students not only understand challenging
+            concepts but also build the confidence they need to thrive
+            academically. There's nothing more rewarding than seeing a student’s
+            “lightbulb moment” and knowing they’re on the path to success. I’d
+            be delighted to support your child’s learning journey. Let’s work
+            together to help them reach their goals!
           </p>
         </div>
       </div>
